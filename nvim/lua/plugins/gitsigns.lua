5733fbec2f2bde923fd9967cbab18bd92dfd5886
Nicholas Kabalan <nkabalan@acvauctions.com>
2024-02-16 15:29:41 -0500

    add keymaps to jump between gitsigns hunk

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -3,12 +3,14 @@ return {
 	event = { "BufReadPost", "BufNewFile" },
 	config = function()
 		require("gitsigns").setup({
-            _extmark_signs=false,
+            _extmark_signs = false,
 			attach_to_untracked = false,
 		})
 
 		local opts = { noremap = true, silent = true }
 		vim.keymap.set("n", "<leader>hr", ":Gitsigns reset_hunk<cr>", opts)
 		vim.keymap.set("n", "<leader>hp", ":Gitsigns preview_hunk<cr>", opts)
+        vim.keymap.set("n", "]c", ":Gitsigns next_hunk<cr>", opts)
+        vim.keymap.set("n", "[c", ":Gitsigns prev_hunk<cr>", opts)
 	end,
 }
