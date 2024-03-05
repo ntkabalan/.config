7248179e1ebffdbab5dbdeac13c201c7a7ad4559
Nicholas Kabalan <nkabalan@acvauctions.com>
2024-03-05 08:39:36 -0500

    format conform.lua

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,20 +1,20 @@
 return {
 	"stevearc/conform.nvim",
-    event = { "BufReadPre", "BufNewFile" },
+	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		local conform = require("conform")
 
 		conform.setup({
 			formatters_by_ft = {
 				go = { "gofmt" },
-                html = { "prettier" },
+				html = { "prettier" },
 				javascript = { "prettier" },
 				javascriptreact = { "prettier" },
 				json = { "prettier" },
 				lua = { "stylua" },
 				markdown = { "prettier" },
-                python = { "isort", "black" },
-                rust = { "rustfmt" },
+				python = { "isort", "black" },
+				rust = { "rustfmt" },
 				typescript = { "prettier" },
 				typescriptreact = { "prettier" },
 				vue = { "prettier" },
