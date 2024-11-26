e40facb8cb58c3fb2eeace26df6ac0931e88c8b4
Nicholas Kabalan <nkabalan@acvauctions.com>
2024-11-26 10:23:52 -0500

    update js/ts language server and formatter config

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -2,22 +2,34 @@ return {
 	"stevearc/conform.nvim",
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
+		local function is_deno_project()
+			return vim.fn.filereadable(vim.fn.getcwd() .. "/deno.json") == 1
+		end
+
+		local js_formatter
+
+		if is_deno_project() then
+			js_formatter = "deno_fmt"
+		else
+			js_formatter = "prettier"
+		end
+
 		local conform = require("conform")
 
 		conform.setup({
 			formatters_by_ft = {
 				go = { "gofmt" },
-				html = { "prettier" },
-				javascript = { "prettier" },
-				javascriptreact = { "prettier" },
-				json = { "prettier" },
+				html = { js_formatter },
+				javascript = { js_formatter },
+				javascriptreact = { js_formatter },
+				json = { js_formatter },
 				lua = { "stylua" },
-				markdown = { "prettier" },
+				markdown = { js_formatter },
 				python = { "isort", "black" },
 				rust = { "rustfmt" },
-				typescript = { "prettier" },
-				typescriptreact = { "prettier" },
-				vue = { "prettier" },
+				typescript = { js_formatter },
+				typescriptreact = { js_formatter },
+				vue = { js_formatter },
 			},
 		})
 
