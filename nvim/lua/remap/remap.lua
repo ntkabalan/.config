f300fd02d1bf62a0c44f34616b87933baf415f18
Not Damon <dmckernan@acvauctions.com>
2025-03-27 12:58:06 -0400

    format

--- nvim/lua/remap/remap.lua
+++ nvim/lua/remap/remap.lua
@@ -21,10 +21,10 @@ vim.keymap.set("v", "<leader>y", '"+y')
 
 -- briefly highlight yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
-    group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 -- find and replace current word
